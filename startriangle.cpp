da49e526b47200609758fa680183d74bb329bb84
Tarun-Sharma-1 <imtarunbharadwaj@gmail.com>
2025-07-10 07:39:23 +0530

    Add files via upload

---
+++ startriangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    system("cls");
+    int row;
+    cout<<"enter row:";
+    cin>>row;
+    for(int i=0;i<=row;i++){
+        for (int j=1;j<=i;j++){
+            cout<<"* ";}
+            cout<<endl;
+    }
+    
+}
